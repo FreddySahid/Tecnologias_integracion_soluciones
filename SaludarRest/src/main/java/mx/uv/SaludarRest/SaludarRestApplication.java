c8ee5d8d65d13c9bdff06f9f5f46271fca9bde75
unknown <freddysahid@outlook.com>
2022-05-19 10:01:12 -0500

    Inicio del proyecto

---
+++ SaludarRest/src/main/java/mx/uv/SaludarRest/SaludarRestApplication.java
@@ -0,0 +1,13 @@
+package mx.uv.SaludarRest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaludarRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaludarRestApplication.class, args);
+	}
+
+}
---
+++ SaludarRest/src/test/java/mx/uv/SaludarRest/SaludarRestApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.uv.SaludarRest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaludarRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
