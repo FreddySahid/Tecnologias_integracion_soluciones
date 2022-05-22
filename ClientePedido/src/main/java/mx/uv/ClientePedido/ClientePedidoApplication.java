6a626f079186cb95225097b5af80117f70e08eb5
unknown <freddysahid@outlook.com>
2022-05-22 11:02:48 -0500

    Inicio del servicio Rest para pedidos

---
+++ ClientePedido/src/main/java/mx/uv/ClientePedido/ClientePedidoApplication.java
@@ -0,0 +1,13 @@
+package mx.uv.ClientePedido;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientePedidoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientePedidoApplication.class, args);
+	}
+
+}
---
+++ ClientePedido/src/test/java/mx/uv/ClientePedido/ClientePedidoApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.uv.ClientePedido;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientePedidoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
